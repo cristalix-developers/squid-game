bc5929a409a416e2033e760c9572d3ad4a0bd764
Tensess <tensessforcristalix1@gmail.com>
2022-08-04 21:34:41 +0500

    совет от максуслика

--- node/src/main/kotlin/me/func/day/play/TntRun.kt
+++ node/src/main/kotlin/me/func/day/play/TntRun.kt
@@ -8,6 +8,7 @@ import me.func.app
 import me.func.day.Day
 import me.func.day.misc.Bonus
 import me.func.util.Music
+import net.minecraft.server.v1_12_R1.BlockGlazedTerracotta
 import org.bukkit.Location
 import org.bukkit.Material
 import org.bukkit.block.Block
@@ -24,8 +25,6 @@ class TntRun(private val game: SquidGame) : Day {
     override val description = "Бегите по блокам, но продержитесь\nдо завершения"
     override val title = "Аккуратность"
     override lateinit var fork: EventContext
-
-    private val blocks = arrayOf(Material.LIGHT_BLUE_GLAZED_TERRACOTTA, Material.RED_GLAZED_TERRACOTTA, Material.WHITE_GLAZED_TERRACOTTA, Material.YELLOW_GLAZED_TERRACOTTA, Material.PINK_GLAZED_TERRACOTTA)
 
     private val bonus = game.map.getLabels("tnt-bonus").map { it.clone().add(0.0, 2.0, 0.0) }
     private val slow = PotionEffect(PotionEffectType.SLOW, 2 * 20, 2)
@@ -58,7 +57,7 @@ class TntRun(private val game: SquidGame) : Day {
         }
 
         fork.on<ProjectileHitEvent> {
-            if (hitBlock != null && blocks.contains(hitBlock.type)) {
+            if (hitBlock != null && hitBlock is BlockGlazedTerracotta) {
                 BlockFace.values().forEach { face -> downgrade(hitBlock.getRelative(face)) }
             }
         }
